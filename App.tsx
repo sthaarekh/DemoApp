068db9c3eebae6e7aae3ec9503f467197458fffe
sthaarekh <arekhshrestha@gmail.com>
2025-04-19 13:18:38 +0545

    Drawer navigation added

--- App.tsx
+++ App.tsx
@@ -1,28 +1,41 @@
 import { Text, View, StyleSheet } from "react-native";
+import 'react-native-reanimated';
+
 import { createStaticNavigation, NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./components/HomeScreen";
 import ProfileScreen from "./components/ProfileScreen";
 import UserScreen from "./components/UserScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-function App(){
+
+const StackNav = ()=>{
   const Stack = createNativeStackNavigator()
   return(
-    <NavigationContainer>
-      <Stack.Navigator
-      initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{
-            title: "Home Screen",
-            headerStyle:{
-              backgroundColor: "orange"
-            },
-            headerTintColor: '#fff'
-            }
-          }
+    <Stack.Navigator
+      initialRouteName="Home" screenOptions={{
+        headerStyle:{
+          backgroundColor: "orange"
+        },
+        headerTintColor: '#fff',
+        headerTitleAlign: 'center',
+        }
+      }>
+        <Stack.Screen name="Home" component={HomeScreen} options={{title: "Home Screen",}}
          />
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="User" component={UserScreen} />
       </Stack.Navigator>
+  )
+}
+
+function App(){
+  const Drawer = createDrawerNavigator();
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   )
 }
